1ff5583f9aa47b3f503172dda6cacd6a67458965
Marlon Suditsch <marlon.suditsch@mechbau.uni-stuttgart.de>
2024-12-16 16:05:52 +0100

    update install_oncogen.sh

--- install_oncogen.sh
+++ install_oncogen.sh
@@ -4,8 +4,10 @@ echo "Choose the environment: (1) for OncoFEM, (2) for OncoGEN"
 read env_choice
 
 if [[ "$env_choice" == "1" ]]; then
+  conda init
   conda activate oncofem
 elif [[ "$env_choice" == "2" ]]; then
+  conda init
   conda activate oncogen
 else
   echo "Invalid choice. Please choose either '1' for OncoSTR or '2' for OncoFEM."
